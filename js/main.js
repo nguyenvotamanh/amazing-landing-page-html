ef72c50a49d8d08b708cd5fc3af486f480db61a4
Tam Anh <nguyenvotamanh@gmail.com>
2019-04-08 17:31:54 +0700

    init

---
+++ js/main.js
@@ -0,0 +1,9 @@
+$(document).ready (function() {
+    
+    new WOW().init();
+    
+    $('.burger-nav').on('click', function () {
+        $('.container nav ul').toggleClass('open')
+    })    
+    
+});
